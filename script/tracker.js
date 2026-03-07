3e3c760d2a6650cfa6db6b3e45c350ef1c917f3d
ajwadhussain-shadman <ajwadhussain38540@gmail.com>
2026-03-08 00:36:14 +0600

    modal function, label function , and responsive done

--- script/tracker.js
+++ script/tracker.js
@@ -2,7 +2,13 @@ const allBtn = document.getElementById("allBtn");
 const openBtn = document.getElementById("openBtn");
 const closedBtn = document.getElementById("closedBtn");
 const issueCardSection = document.getElementById("issueCardSection");
+const modalTitle = document.getElementById("modalTitle");
+const modalLabels = document.getElementById("modalLabels");
+const modalDescription = document.getElementById("modalDescription");
+const modalAssignee = document.getElementById("modalAssignee");
+const modalPriority = document.getElementById("modalPriority");
 let issueNumber = document.getElementById("issueNumber");
+const issueModal=document.getElementById("issueModal");
 const states = [allBtn, openBtn, closedBtn];
 let cState = allBtn;
 function currentState(id) {
@@ -35,23 +41,23 @@ function displayIssues(issues) {
     issueCardSection.innerHTML = "";
     issues.forEach(issue => {
         let statusIcon = "";
-        let priorityBgColor="";
-        let priorityTextColor="";
-        if(issue.priority==="high"){
-           priorityBgColor="bg-[#FEECEC]";
-           priorityTextColor="text-[#EF4444]";
+        let priorityBgColor = "";
+        let priorityTextColor = "";
+        if (issue.priority === "high") {
+            priorityBgColor = "bg-[#FEECEC]";
+            priorityTextColor = "text-[#EF4444]";
         }
-        else if(issue.priority==="medium"){
-            priorityBgColor="bg-[#FFF6D1]";
-           priorityTextColor="text-[#F59E0B]";
+        else if (issue.priority === "medium") {
+            priorityBgColor = "bg-[#FFF6D1]";
+            priorityTextColor = "text-[#F59E0B]";
         }
         else {
-             priorityBgColor="bg-[#EEEFF2]";
-           priorityTextColor="text-[#9CA3AF]";
+            priorityBgColor = "bg-[#EEEFF2]";
+            priorityTextColor = "text-[#9CA3AF]";
         }
-            
-        issue.status === "open" ?statusIcon = "./assets/Open-Status.png" : statusIcon ="./assets/Closed-Status.png";
-        
+
+        issue.status === "open" ? statusIcon = "./assets/Open-Status.png" : statusIcon = "./assets/Closed-Status.png";
+
         if (cState === openBtn) {
             if (issue.status !== "open") {
                 return;
@@ -65,55 +71,17 @@ function displayIssues(issues) {
 
         let labelHtml = "";
         issue.labels.forEach(label => {
-            if (label === "bug") {
-                labelHtml += `
-              <div
-                        class="label-1 flex  items-center bg-[#FEECEC] border border-[#FECACA] rounded-[100px] w-14 justify-center gap-0.5">
-                        <img src="./assets/BugDroid.png" alt="" class="">
-                        <p class="text-[#EF4444] text-[12px] font-medium">Bug</p>
-                    </div>
-             `
-            }
-            else if (label === "help wanted") {
-                labelHtml += `   <div
-                        class="label-2 flex  items-center bg-[#FFF8DB] border border-[#FDE68A] rounded-[100px] w-28 justify-center gap-0.5">
-                        <img src="./assets/Lifebuoy.png" alt="" class="">
-                        <p class="text-[#D97706] text-[12px] font-medium">HELP WANTED</p>
-                    </div>`
-            }
-            else if (label === "enhancement") {
-                labelHtml += `<div
-                        class="label-3 flex  items-center bg-[#DEFCE8] border border-[#BBF7D0] rounded-[100px] w-28 justify-center gap-0.5">
-                        <img src="./assets/Sparkle.png" alt="" class="">
-                        <p class="text-[#00A96E] text-[12px] font-medium">Enhancement</p>
-                    </div>`
-            }
-            else if(label==="good first issue"){
-                labelHtml += `<div
-                        class="label-3 flex  items-center bg-[#DEFCE8] border border-[#BBF7D0] rounded-[100px] w-32 justify-center gap-0.5">
-                        <img src="./assets/Sparkle.png" alt="" class="">
-                        <p class="text-[#00A96E] text-[12px] font-medium">good first issue</p>
-                    </div>`
-            }
-             else if(label==="documentation"){
-                labelHtml += `<div
-                        class="label-3 flex  items-center bg-[#DEFCE8] border border-[#BBF7D0] rounded-[100px] w-28 justify-center gap-0.5">
-                        <img src="./assets/Sparkle.png" alt="" class="">
-                        <p class="text-[#00A96E] text-[12px] font-medium">documentation</p>
-                    </div>`
-            }
-
+         labelHtml+=buildLabel(label);
         });
-
         let issueCard = document.createElement("div");
-        issueCard.classList.add("card", "w-[256px]", "shadow-md", "bg-[#ffffff]", "rounded-lg", "border-t-4", `${issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"}`);
+        issueCard.classList.add("card", "w-full", "shadow-md", "bg-[#ffffff]", "rounded-lg", "border-t-4", `${issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"}`);
         issueCard.innerHTML = `
              <div class="uppersection p-6 space-y-3">
                           
-                <div class="card-top flex justify-between">
+                <div class= "card-top flex justify-between">
                     <!-- left icon -->
                     <div class="left-icon">
-                        <img src=${statusIcon} alt="">
+                        <img src="${statusIcon}"alt="">
                     </div>
                     <!-- priority badge -->
                     <div class="priority-badge ${priorityBgColor} rounded-[100px] w-20 flex justify-center items-center">
@@ -140,6 +108,82 @@ function displayIssues(issues) {
         `
         issueCardSection.appendChild(issueCard);
         issueNumber.innerText = issueCardSection.children.length;
+        issueCard.addEventListener("click",()=>{
+            showIssueModal(issue.id);
+        })
     })
 }
+
+async function showIssueModal(id) {
+    const res=await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data=await res.json();
+    // console.log(data);
+    const issue=data.data;
+    modalTitle.innerText=issue.title;
+    modalDescription.innerText=issue.description;
+    modalAssignee.innerText=issue.assignee;
+    modalPriority.innerText=issue.priority;
+    modalLabels.innerHTML="";
+    
+    if (issue.priority === "medium") {
+            modalPriority.classList.remove("bg-[#EF4444]","bg-[#9CA3AF]")
+            modalPriority.classList.add("bg-[#D97706]");
+        }
+        else if(issue.priority==="high"){
+              modalPriority.classList.remove("bg-[#9CA3AF]","bg-[#D97706]")
+            modalPriority.classList.add("bg-[#EF4444]");
+        }
+        else if(issue.priority==="low"){
+             modalPriority.classList.remove("bg-[#EF4444]","bg-[#D97706]")
+            modalPriority.classList.add("bg-[#9CA3AF]");
+        }
+
+        let labelHtml = "";
+        issue.labels.forEach(label => {
+         labelHtml+=buildLabel(label);
+        });
+        modalLabels.innerHTML+=labelHtml;
+
+    issueModal.showModal();
+}
+function buildLabel(label){
+    if (label === "bug") {
+                return `
+              <div
+                        class="label-1 flex  items-center bg-[#FEECEC] border border-[#FECACA] rounded-[100px] w-14 justify-center gap-0.5">
+                        <img src="./assets/BugDroid.png" alt="" class="">
+                        <p class="text-[#EF4444] text-[12px] font-medium">Bug</p>
+                    </div>
+             `
+            }
+            else if (label === "help wanted") {
+                return `   <div
+                        class="label-2 flex  items-center bg-[#FFF8DB] border border-[#FDE68A] rounded-[100px] w-28 justify-center gap-0.5">
+                        <img src="./assets/Lifebuoy.png" alt="" class="">
+                        <p class="text-[#D97706] text-[12px] font-medium">HELP WANTED</p>
+                    </div>`
+            }
+            else if (label === "enhancement") {
+                return`<div
+                        class="label-3 flex  items-center bg-[#DEFCE8] border border-[#BBF7D0] rounded-[100px] w-28 justify-center gap-0.5">
+                        <img src="./assets/Sparkle.png" alt="" class="">
+                        <p class="text-[#00A96E] text-[12px] font-medium">Enhancement</p>
+                    </div>`
+            }
+            else if (label === "good first issue") {
+                return `<div
+                        class="label-3 flex  items-center bg-[#DEFCE8] border border-[#BBF7D0] rounded-[100px] w-32 justify-center gap-0.5">
+                        <img src="./assets/Sparkle.png" alt="" class="">
+                        <p class="text-[#00A96E] text-[12px] font-medium">good first issue</p>
+                    </div>`
+            }
+            else if (label === "documentation") {
+                return `<div
+                        class="label-3 flex  items-center bg-[#DEFCE8] border border-[#BBF7D0] rounded-[100px] w-28 justify-center gap-0.5">
+                        <img src="./assets/Sparkle.png" alt="" class="">
+                        <p class="text-[#00A96E] text-[12px] font-medium">documentation</p>
+                    </div>`
+            }
+}
+
 loadIssues();
